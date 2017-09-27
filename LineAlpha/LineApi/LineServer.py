9c081d8075126555fd6315348a36b9d57b138356
kirwinkk <31506555+kirwinkk@users.noreply.github.com>
2017-09-27 18:48:11 +0800

    Update LineServer.py

--- LineAlpha/LineApi/LineServer.py
+++ LineAlpha/LineApi/LineServer.py
@@ -31,8 +31,8 @@ class url(object):
     UserAgent   = 'DESKTOP:MAC:10.10.2-YOSEMITE-x64(4.5.0)'
     AppName     = 'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0'
     port        = 443
-    systemname  = 'BG'
-    ip          = '155.94.159.19'
+    systemname  = 'Battle-God'
+    ip          = '8.8.8.8'
     _session = requests.session()
     Headers = {}
     _pincode = None
